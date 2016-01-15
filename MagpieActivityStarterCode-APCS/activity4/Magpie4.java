3cd5411a58b8b5a79a569e944e919305ae79d700
hoarseradish <trex_gavin@yahoo.com>
2016-01-15 08:49:05 -0800

    1/15

--- MagpieActivityStarterCode-APCS/activity4/Magpie4.java
+++ MagpieActivityStarterCode-APCS/activity4/Magpie4.java
@@ -55,6 +55,11 @@ public class Magpie4
 		{
 			response = transformIWantToStatement(statement);
 		}
+		
+		else if (findKeyword(statement, "I want", 0) >= 0)
+		{
+			response = transformIWantStatement(statement);
+		}
 
 		else
 		{
@@ -97,7 +102,27 @@ public class Magpie4
 		return "What would it mean to " + restOfStatement + "?";
 	}
 
-	
+	/**
+	 * Take a statement with "I want <something>." and transform it into 
+	 * "Would you really be happy if you had <something>?"
+	 * @param statement the user statement, assumed to contain "I want"
+	 * @return the transformed statement
+	 */
+	private String transformIWantStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
+	}
 	
 	/**
 	 * Take a statement with "you <something> me" and transform it into 
